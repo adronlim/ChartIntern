081b3ae0d697339d89c7a120512e581b28128877
Lim Min Jie <adronlim@hotmail.com>
2019-07-24 16:43:45 +0800

    ERROR ReferenceError: $ is not defined[Pending]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, OnChanges, OnDestroy, OnInit, SimpleChanges} from '@angular/core';
+import {Router, VERSION} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit, OnChanges, OnDestroy {
   title = 'ChartIntern';
+  IdChartComponent: any;
+  Id_Chart_Component: any;
+  OnClick: boolean;
+  name: any;
+
+  constructor(private router: Router) {
+    this.name = `Angular! v${VERSION.full}`;
+  }
+  ngOnInit() {
+    console.log('ngOnint \n'/*, this.IdChartComponent*/);
+    // this.loadModule();
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('%cinput changed!!!!!!!!!!'/*, 'background: #222; color: #bada55'*/);
+    console.log(changes);
+  }
+  ngOnDestroy(): void {
+    this.IdChartComponent = null;
+  }
+  ngAfterViewInit(): void {
+    console.log('ngAfterVuewInt \n'/*, this.IdChartComponent*/);
+    // this.Id_Chart_Component = this.IdChartComponent;
+  }
+  // loadModule() {
+  //   this.router.resetConfig([
+  //     ...this.router.config,
+  //     {
+  //       path: 'dynamicModule',
+  //       loadChildren: './dynamic-component.module.ts#DynamicComponentModule'
+  //     }
+  //   ]);
+  //
+  //   this.router.navigateByUrl('dynamicModule/dynamicComponent1');
+  // }
+  getChartCompID(e: any) {
+    this.IdChartComponent = e;
+    console.log('getChartCompID \n', e);
+  }
+  getClickTrigger(e: any) {
+    this.OnClick = e;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,98 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {
+  AgmCoreModule
+} from '@agm/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {PanelLayoutComponent} from './panel-layout/panel-layout.component';
+import {ChartDirective} from './chart-directive.directive';
+import {NavBarComponent} from './nav-bar/nav-bar.component';
+import {BarChartComponent} from './bar-chart/bar-chart.component';
+import {PieChartComponent} from './pie-chart/pie-chart.component';
+import {HistogramChartComponent} from './histogram-chart/histogram-chart.component';
+import {StackedBarChartComponent} from './stacked-bar-chart/stacked-bar-chart.component';
 
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {SideBarComponent} from './side-bar/side-bar.component';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {ServicesService} from './services.service';
+import {ChartModule} from './chart/chart.module';
+// import {HomePageComponent} from './home-page/home-page.component';
+import {RouterModule, Routes} from '@angular/router';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import {ComponentsModule} from './components/components.module';
+import {AppRoutingModule} from './app-routing.module';
+import {ChartComponent} from './chart/chart.component';
+import {CommonModule} from '@angular/common';
+
+// const appRoutes: Routes = [
+//   // { path: 'crisis-center', component: CrisisListComponent },
+//   // { path: 'hero/:id',      component: HeroDetailComponent },
+//   {
+//     path: 'home',
+//     component: AppComponent,
+//     data: {title: 'Heroes List'}
+//   },
+//   {
+//     path: '1',
+//     redirectTo: '/home',
+//     pathMatch: 'full'
+//   }
+//   // , { path: '**', component: PageNotFoundComponent }
+// ];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChartDirective,
+
+    // PanelLayoutComponent,
+    // NavBarComponent,
+    // DashboardComponent,
+    // SideBarComponent,
+    // BarChartComponent,
+    // PieChartComponent,
+    // HistogramChartComponent,
+    // StackedBarChartComponent,
+    // HomePageComponent,
+    AdminLayoutComponent,
+
   ],
   imports: [
-    BrowserModule
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserModule,
+    ComponentsModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule,
+    CommonModule,
+    // .forRoot(
+    //   appRoutes,
+    //   {enableTracing: true} // <-- debugging purposes only
+    // ),
+    AgmCoreModule.forRoot({
+      // apiKey: 'YOUR_GOOGLE_MAPS_API_KEY'
+    }),
   ],
   providers: [],
+  // entryComponents: [
+  //   BarChartComponent,
+  //   PieChartComponent,
+  //   HistogramChartComponent,
+  //   StackedBarChartComponent
+  // ]
+  // ,
+  exports: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  static forRoot() {
+    return {
+      ngModule: AppModule,
+      providers: [ServicesService, ChartDirective]
+    };
+  }
+}
